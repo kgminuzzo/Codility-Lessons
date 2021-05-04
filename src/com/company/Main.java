344ea2d39bc8ebe528aee4c8c934ee0d9845ec46
karina_minuzzo <karina_minuzzo@dell.com>
2021-05-04 20:49:08 -0300

    New lessons with 100% score achieved

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,25 +3,39 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        // BINARY GAP
+        // BINARY GAP (100%)
         //System.out.println(new BinaryGap().solution(304));
 
-        //CYCLIC ROTATION
+        //CYCLIC ROTATION (100%)
         //System.out.println(Arrays.toString(new CyclicRotation().solution(new int[]{}, 1)));
 
-        //ODD NUMBERS
+        //ODD NUMBERS (100%)
         //System.out.print(new OddNumbers().solution(new int[]{9,3,9,3,9,7,9}));
 
-        //FROG JUMPS
+        //FROG JUMPS (100%)
         //System.out.print(new FrogJumps().solution(30,40,0));
 
-        //PERM MISSING ELEMENT
+        //PERM MISSING ELEMENT (100%)
         //System.out.println(new PermMissingElem().solution(new int[]{5,1,2,3}));
 
-        //TAPE EQUILIBRIUM
+        //TAPE EQUILIBRIUM (100%)
         //System.out.println(new TapeEquilibrium().solution(new int[]{2000,100}));
 
-        //FROG RIVER
-        System.out.println(new FrogRiverOne().solution(1, new int[] {1}));
+        //FROG RIVER (100%)
+        //System.out.println(new FrogRiverOne().solution(1, new int[] {1}));
+
+        //TODO
+        //COUNT DIV
+        System.out.println(new CountDiv().solution(10,30,7));
+
+        //PERMUTATION CHECK (100%)
+        //System.out.println(new PermCheck().solution(new int[] {4, 2, 2, 2}));
+
+        //TODO
+        //MAX COUNTERS
+        //System.out.println(new MaxCounters().solution(10,new int[] {4, 2, 2, 2}));
+
+        //MISSING INTEGER (100%) https://app.codility.com/demo/results/trainingGY9VVF-VDD/
+        //System.out.println(new MissingInteger().solution(new int[] {-1,2}));
     }
 }
